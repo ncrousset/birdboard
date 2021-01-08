7e28ad247afeaf8c46a2d760feb3f649d439bd17
Rudys Natanael Acosta Crousset <natanael926@gmail.com>
2021-01-07 19:41:59 -0500

    Fixed the layout error

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,39 +1,47 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="bg-gray-100">
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+<body class="bg-gray-100">
 
-        <nav class="bg-white">
-            <div class="container mx-auto">
-                <div class="flex justify-between items-center py-2">
-                    <h1>
-                        <a href="{{ url('/') }}" class="navbar-brand">
-                            <img src="/images/logo.svg" alt="Birdboard" >
-                        </a>
-                    </h1>
-                </div>
+@if(auth()->guest())
+    <div class="font-sans text-gray-900 antialiased">
+        {{ $slot }}
+    </div>
+@else
+    <nav class="bg-white">
+        <div class="container mx-auto">
+            <div class="flex justify-between items-center py-2">
+                <h1>
+                    <a href="{{ url('/') }}" class="navbar-brand">
+                        <img src="/images/logo.svg" alt="Birdboard">
+                    </a>
+                </h1>
             </div>
-        </nav>
+        </div>
+    </nav>
 
 
-        <main class="container py-4 mx-auto">
-            @yield('content')
-        </main>
+    <main class="container py-4 mx-auto">
+        @yield('content')
+    </main>
 
-    </body>
+@endif
+
+
+</body>
 </html>
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <header class="flex items-center mb-3 py-4">
-        <div class="flex justify-between items-center w-full">
+        <div class="flex justify-between items-end w-full">
             <h2 class="text-gray-500 text-sm font-normal">My Projects</h2>
             <a href="/projects/create" class="button text-white no-underline text-sm py-2 px-5">New Project</a>
         </div>
