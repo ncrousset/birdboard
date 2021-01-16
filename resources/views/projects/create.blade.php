ca87417172894fb54f4a93756c5e501731bec37f
Rudys Natanael Acosta Crousset <natanael926@gmail.com>
2021-01-15 19:25:20 -0500

    Edit create project view

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,22 +1,38 @@
 @extends('layouts.guest')
 
 @section('content')
-    <form method="post" action="/projects" class="container">
+    <form
+        method="post"
+        action="/projects"
+        class="lg:w-1/2 lg:mx-auto bg-white p-6 md:py-12 md:px-12 rounded shadow">
         @csrf
+        <h1 class="text-2xl font-normal mb-10 text-center">
+            Let's start something new
+        </h1>
 
-        <h1 class="heading is-1">Create a Project</h1>
-        <div class="field">
-            <label for="title" class="title">Title</label>
+
+
+        <div class="field mb-6">
+            <label class="label text-sm mb-2 block" for="title">Title</label>
             <div class="control">
-                <input type="text" class="input" name="title" placeholder="Title">
+                <input
+                    type="text"
+                    class="input bg-transparent border border-gray-light rounded p-2 text-xs w-full focus:outline-none"
+                    name="title"
+                    placeholder="My next awesome project">
             </div>
         </div>
 
-        <div class="field">
-            <label for="description" class="label">Description</label>
+        <div class="field mb-6">
+            <label class="label text-sm mb-2 block"  for="description" >Description</label>
 
             <div class="control">
-                <textarea name="description" class="textarea"></textarea>
+                <textarea
+                    name="description"
+                    rows="5"
+                    placeholder=""
+                    class="textarea bg-transparent border border-gray-light rounded p-2 text-xs w-full focus:outline-none">
+                </textarea>
             </div>
         </div>
 
